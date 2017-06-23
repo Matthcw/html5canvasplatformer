84dae0f4c2c8b844a0451999c13098ccce82e1d5
M.P.S.Mukalere <m.p.s.mukalere@kent.ac.uk>
2017-06-23 14:22:06 +0100

    Collision working all around

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var playerY = 0;
 var playerSpeedY = 0;
 var playerSpeedX = 0;
 const PLAYER_RADIUS = 25;
+const PLAYER_RADIUS_MARGIN = PLAYER_RADIUS + 5;
 const PLAYER_SPEED = 4;
 const PLAYER_JUMP = -10;
 
@@ -25,16 +26,16 @@ var keyDownPressed = false;
 
 
 var levelArray = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                  1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-                  0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0,
+                  1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                  1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1,
+                  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1,
+                  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                  1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1,
+                  1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                  1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                  1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+                  1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1,
+                  1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1,
                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
   var mousePos = {x:0,y:0};
@@ -101,19 +102,32 @@ var levelArray = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     playerX += playerSpeedX;
     playerY += playerSpeedY;
     if(keyLeftPressed){
-      console.log("yh");
-      playerSpeedX = -PLAYER_SPEED;
+      if(playerIsAtBlock(playerX - PLAYER_RADIUS_MARGIN, playerY)){
+        playerSpeedX = 0;
+      } else {
+        playerSpeedX = -PLAYER_SPEED;
+      }
     }
+
     if(keyRightPressed){
-      playerSpeedX = PLAYER_SPEED;
+      if(playerIsAtBlock(playerX + PLAYER_RADIUS_MARGIN, playerY)){
+        playerSpeedX = 0;
+      } else {
+        playerSpeedX = PLAYER_SPEED;
+      }
     }
+
     if(!keyLeftPressed && !keyRightPressed){
       playerSpeedX = 0;
+    }
+
+    if(playerIsAtBlock(playerX, playerY - PLAYER_RADIUS_MARGIN)){
+      playerSpeedY = 0.2;
     }
     //Handle collision of ball, if there's no platform below it, it'll drop
     //How do we know whether there is a blue block at this coordinate? - TILES
     //playerY *=1.04;
-    if(playerIsAtBlock(playerX, playerY + PLAYER_RADIUS) == 1){
+    if(playerIsAtBlock(playerX, playerY + PLAYER_RADIUS_MARGIN)){
       if(keyUpPressed){
         playerSpeedY = PLAYER_JUMP;
       } else {
@@ -127,7 +141,7 @@ var levelArray = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
   function playerIsAtBlock(xPos, yPos){
     var currentCol = Math.floor(xPos / BLOCK_W);
     var currentRow = Math.floor(yPos / BLOCK_H);
-    return levelArray[currentCol + currentRow * BLOCK_COLS];
+    return levelArray[currentCol + currentRow * BLOCK_COLS] == 1;
   }
 
   function drawLevel(){
